a42efecb6b9235b8e92194d53bddbbcdbf90f44f
DcWind <danielekwuazi@gmail.com>
2016-12-28 05:47:28 +0700

    added details to app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { AboutPage } from '../pages/about/about';
 import { RedditPage } from '../pages/reddit/reddit';
+import { DetailsPage } from '../pages/details/details';
 import { SettingsPage } from '../pages/settings/settings';
 import { TabsPage } from '../pages/tabs/tabs';
 
@@ -11,6 +12,7 @@ import { TabsPage } from '../pages/tabs/tabs';
     MyApp,
     AboutPage,
     RedditPage,
+    DetailsPage,
     SettingsPage,
     TabsPage
   ],
@@ -22,6 +24,7 @@ import { TabsPage } from '../pages/tabs/tabs';
     MyApp,
     AboutPage,
     RedditPage,
+    DetailsPage,
     SettingsPage,
     TabsPage
   ],
